0c5bde71b935f4398c2ccd073250342a1517b046
qanqanqan <fortissimusoffi@gmail.com>
2024-06-10 21:37:29 +0400

    Added basic models

--- wsite/models.py
+++ wsite/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
+
+class WishlistPosition(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    links = models.JSONField(default=list, blank=True)
+    image = models.ImageField(upload_to='positions/%Y/%m/%d/', blank=True)
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
